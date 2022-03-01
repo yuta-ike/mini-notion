291ee5f74e3f6bbcda239cf1bed413fb47a4eddf
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-03-01 14:09:52 +0900

    apply prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import type { AppProps } from 'next/app'
-import '../styles/globals.css'
+import type { AppProps } from "next/app"
+import "../styles/globals.css"
 
 const App: React.VFC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
-import { NextPage } from "next";
+import { NextPage } from "next"
 
 const Index: NextPage = () => {
-  return (
-    <div>Hello, world</div>
-  )
+  return <div>Hello, world</div>
 }
 
 export default Index
