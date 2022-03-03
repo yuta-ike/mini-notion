bc864a9e979a17d6cf78fee5008517e3cef9e6c8
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-03-04 01:57:15 +0900

    add encodeURIComponent

--- src/utils/base64/encodeBase64.ts
+++ src/utils/base64/encodeBase64.ts
@@ -4,9 +4,9 @@ const textEncoder = new TextEncoder()
 const textDecoder = new TextDecoder()
 
 export const encodeBase64 = (source: string) => {
-  return fromByteArray(textEncoder.encode(source))
+  return fromByteArray(textEncoder.encode(encodeURIComponent(source)))
 }
 
 export const decodeBase64 = (hash: string) => {
-  return textDecoder.decode(toByteArray(hash))
+  return decodeURIComponent(textDecoder.decode(toByteArray(hash)))
 }
