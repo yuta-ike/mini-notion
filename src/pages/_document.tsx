239ecf69f38481862d7696b722510bf8610cdc7d
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-03-04 17:49:25 +0900

    add meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,7 @@ const Document = () => {
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap"
           rel="stylesheet"
         />
+        <meta name="robots" content="noindex nofollow" />
       </Head>
       <body>
         <Main />
